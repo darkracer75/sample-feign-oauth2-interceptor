2c9d5779510ad2e2b8cbc6c842b897059b0ba060
Andre de Fontana Ignacio <andre.ignacio@netshoes.com>
2018-01-08 10:18:41 -0200

    PersonController renamed to ApplicationController

--- src/main/java/com/sample/feign/oauth2/interceptor/ApplicationController.java
+++ src/main/java/com/sample/feign/oauth2/interceptor/ApplicationController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
-public class PersonController {
+public class ApplicationController {
 
     private PersonFeignClient personFeignClient;
     private OrderFeignClient orderFeignClient;
